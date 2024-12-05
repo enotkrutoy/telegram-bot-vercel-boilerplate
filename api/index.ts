51684a3409cfb54cf55ba300d5eb9cfcd51bcdc5
enotkrutoy <judithabusufaitdyg30@gmail.com>
2024-12-05 17:29:41 -0500

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,15 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import { startVercel } from '../src';
-
-export default async function handle(req: VercelRequest, res: VercelResponse) {
-  try {
-    await startVercel(req, res);
-  } catch (e: any) {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
-    console.error(e.message);
-  }
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "vue-static/package.json",
+      "use": "@vercel/node"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "vue-static/$1"
+    }
+  ]
 }
